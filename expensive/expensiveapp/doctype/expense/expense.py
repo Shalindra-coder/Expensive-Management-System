733eac8ecbc2b78fb55174b2e7af947e1edaff23
Shalindra-coder <shalindraaporiya23@navgurukul.org>
2024-10-05 23:43:46 +0530

    add

---
+++ expensive/expensiveapp/doctype/expense/expense.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024, Shalindra and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class Expense(Document):
+  def validate(self):
+        if self.amount <= 0:
+            frappe.throw("Amount must be greater than zero.")
+
